da99fe6336f9fc6bfa4c48752bf14b691a34fbfd
lostlemoncurd <109440184+LostLemonCurd@users.noreply.github.com>
2023-03-07 16:23:24 +0100

    Ajout table message

--- index.sql
+++ index.sql
@@ -2,7 +2,6 @@ CREATE DATABASE chrysalide;
 
 USE chrysalide;
 
-
 CREATE TABLE user (
     id_user INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(50),
@@ -36,4 +35,11 @@ CREATE TABLE commentaire (
     id_user INT NOT NULL,
     content VARCHAR(500),
     PRIMARY KEY (id_comm)
+) ENGINE=InnoDB;
+
+CREATE TABLE mp (
+    id_msg INT NOT NULL AUTO_INCREMENT,
+    id_user INT NOT NULL,
+    content VARCHAR(1000),
+    PRIMARY KEY (id_msg)
 ) ENGINE=InnoDB;
