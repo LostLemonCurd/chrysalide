9e7bb4502ed2ff260e5f8a56f03f8a4146323f49
lostlemoncurd <109440184+LostLemonCurd@users.noreply.github.com>
2023-04-04 10:24:23 +0200

    Push push

--- script.js
+++ script.js
@@ -2,3 +2,21 @@ let logo = document.querySelector('#header-logo > img');
 logo.addEventListener('click', () => {
     location.href('index.php');
 })
+
+
+let friend = document.querySelectorAll('.friend');
+console.log(friend);
+let friendDetail = document.querySelector('.friend-detail');
+let count = 1;
+
+
+for (let index = 0; index < friend.length; index++) {
+    friend[index].addEventListener('click', () => {
+        if (count % 2 == 0){
+            friendDetail.style.display = 'flex';
+        } else {
+            friendDetail.style.display = 'none';
+        }
+        count++;
+    })
+}
