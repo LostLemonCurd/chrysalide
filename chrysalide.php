7bb74d28e66fb0773ee7052e7f3d3a4240786a9c
Lozam2001 <119404381+Lozam2001@users.noreply.github.com>
2023-04-05 16:04:37 +0200

    overlay

--- chrysalide.php
+++ chrysalide.php
@@ -7,6 +7,7 @@
     <link rel="stylesheet" href="reset.css">
     <link rel="stylesheet" href="lib.css">
     <link rel="stylesheet" href="chysalide.css">
+    <link rel="stylesheet" href="création-chrysalide.php">
     <script src='lib.js' defer></script>
   <title>Chrysalide</title>
 </head>
@@ -86,27 +87,122 @@
                 </div>
             </div>
             <div id="btn-box">
-                <button class="btn-blue"><h6>Créer</h6></button>
+                <button class="btn-blue" onclick="openOverlay()"><h6>Créer</h6></button>
                 <button class="btn-vide"><h6>Rejoindre</h6></button>
             </div>
-            
-                  <button class="btn-inviter"><h6>Inviter</h6></button>
-                  <button class="btn-blue"><h6>Créer</h6></button>
+            <?php
+include('init.php');
+// Vérifier si le formulaire a été soumis
+$user_id = $_SESSION["user"]["id_user"];
+if ($_POST) {
+
+    // Récupérer les données saisies par l'utilisateur
+    $ville = $_POST["ville"];
+    $places_disponibles = $_POST["places_disponibles"];
+    $sport = $_POST["sports"];
+
+
+    // Générer un nom de groupe aléatoire
+    $nom_groupe = 'Groupe_' . uniqid();
+
+    // Vérifier si le nom de groupe est déjà utilisé
+    $r = $pdo->query("SELECT * FROM groupes WHERE nom_groupe = '$nom_groupe'");
+    if ($r->rowCount() >= 1) {
+        // Le nom de groupe est déjà utilisé, générer un nouveau nom
+        $nom_groupe = 'Groupe_' . uniqid();
+    }
+
+    // Insérer les données dans la table "groupes"
+    $pdo->exec("INSERT INTO groupes (nom_groupe, ville, places_disponibles, sport, id_user) 
+    VALUES ('$nom_groupe', '$ville', '$places_disponibles', '$sport', '$user_id')");
+
+
+    if (mysqli_query($connexion, $requete)) {
+        echo "Données insérées avec succès";
+    } else {
+        echo "Erreur : " . $requete . "<br>" . mysqli_error($connexion);
+    }
+
+   
+}
+?>
+            <div class="overlay">
+    <div class="overlay-content">
+    <h1 id="titre-overlay">Créer ta Chrysalide</h1>
+    <button class="close-btn">x</button>
+        <form method="post" action="<?php echo htmlspecialchars($_SERVER["chrysalide"]); ?>">
+            <h2 class='espace'>Ville : </h2><input type="text" name="ville"><br>
+            <h2 class='espace'>Places disponibles : </h2>
+            <select name="places_disponibles">
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+                <option value="6">6</option>
+                <option value="7">7</option>
+                <option value="8">8</option>
+                <option value="9">9</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+            </select><br>
+            <h2 class="espace">Sports :</h2><br>
+            <div class="sport_line">
+            <div class="carte_sport">
+                <img src="img/foot.png" alt="" width="112" height="100">
+                <input type="checkbox" name="sport[]" value="Football">Football<br>
             </div>
+            <div class="carte_sport">
+                <img src="img/foot.png" alt="" width="112" height="100">
+                <input type="checkbox" name="sport[]" value="Basketball">Basketball<br>
+            </div>
+            <div class="carte_sport">
+                <img src="img/foot.png" alt="" width="112" height="100">
+                <input type="checkbox" name="sport[]" value="Golf">Golf<br>
+            </div>
+            <div class="carte_sport">
+                <img src="img/foot.png" alt="" width="112" height="100">
+                <input type="checkbox" name="sport[]" value="Boxe">Boxe<br>
+            </div>
+            </div>
+            <div class="sport_line">
+            <div class="carte_sport">
+                <img src="img/foot.png" alt="" width="112" height="100">
+                <input type="checkbox" name="sport[]" value="Baseball">Baseball<br>
+            </div>
+            <div class="carte_sport">
+                <img src="img/foot.png" alt="" width="112" height="100">
+                <input type="checkbox" name="sport[]" value="Tennis">Tennis<br>
+            </div>
+            <div class="carte_sport">
+                <img src="img/foot.png" alt="" width="112" height="100">
+                <input type="checkbox" name="sport[]" value="Ping-pong">Ping-pong<br>
+            </div>
+            <div class="carte_sport">
+                <img src="img/foot.png" alt="" width="112" height="100">
+                <input type="checkbox" name="sport[]" value="Badminton">Badminton<br>
+            </div>
+            </div>
+            </select>
+            <div class="Créergroupe"><input class=btn-blue type="submit" value="Créer un groupe"></div>
+            
+        </form>
+    </div>
+</div>
             <script>
-                const btnBlue = document.querySelector('.btn-blue');
+                const createBtn = document.querySelector('.btn-blue');
                 const overlay = document.querySelector('.overlay');
                 const closeBtn = document.querySelector('.close-btn');
 
-                btnBlue.addEventListener('click', function() {
-                overlay.style.display = 'block';
+                createBtn.addEventListener('click', () => {
+                    overlay.style.display = 'block';
                 });
 
-                closeBtn.addEventListener('click', function() {
-                overlay.style.display = 'none';
+                closeBtn.addEventListener('click', () => {
+                    overlay.style.display = 'none';
                 });
             </script>
-            </div>
             <div id="block-checkbox">
                 <label for="public">Public</label>
                 <input type="checkbox" id="public" name="access" value="public">
@@ -303,8 +399,6 @@
         </section>
 
     </main>
-
-    
 
     <script>
         // Récupérer les deux checkboxes
