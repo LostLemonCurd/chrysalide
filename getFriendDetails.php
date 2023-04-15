ba86983ea6de11856de2eb32723609f787a53a58
lostlemoncurd <109440184+LostLemonCurd@users.noreply.github.com>
2023-04-15 18:59:10 +0200

    Updated getFriendDetails so that it checks all

--- getFriendDetails.php
+++ getFriendDetails.php
@@ -16,15 +16,18 @@ header('Content-Type: application/json');
 
 
 // Récupérer toutes les informations des utilisateurs sur lesquelles on clique
-$r4 = $pdo->prepare("SELECT * FROM user WHERE id_user = ?");
-$r4->execute([$_GET['friendId']]);
-$friend = $r4->fetch(PDO::FETCH_ASSOC);
+$r1 = $pdo->prepare("SELECT * FROM user WHERE id_user = ?");
+$r1->execute([$_GET['friendId']]);
+$friend = $r1->fetch(PDO::FETCH_ASSOC);
 
 // Vérification du statut d'amitié de l'utilisateur connecté avec les autres utilisateurs
 $isAFriend = false;
-$r3 = $pdo->prepare("SELECT * FROM friends WHERE id_user = ? AND id_friend = ?");
-$r3->execute([$_GET['userId'], $_GET['friendId']]);
-if ($r3->rowCount() >= 1) {
+$r2 = $pdo->prepare("SELECT * FROM friends WHERE (id_user = ? AND id_friend = ?)");
+$r2->execute([$_GET['userId'], $_GET['friendId']]);
+$r3 = $pdo->prepare("SELECT * FROM friends WHERE (id_user = ? AND id_friend = ?)");
+$r3->execute([$_GET['friendId'], $_GET['userId']]);
+
+if (($r2->rowCount() >= 1) or ($r3->rowCount() >= 1)) {
     $isAFriend = true;
 }
 
